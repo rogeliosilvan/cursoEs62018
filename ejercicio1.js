9ebba6c24b7325f6d947a7613a7198c279374252
Rogelio Silvan Perez <rogoffspring@hotmail.com>
2018-10-02 11:00:19 +0200

    ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+function mostrarNac ()
+{
+    var newYear = new Date(1980, 5, 11);
+    console.log(`Naci el dia ${newYear.getDate()} , el mes ${newYear.getMonth()+1} y el año ${newYear.getFullYear()}`);
+    
+}
