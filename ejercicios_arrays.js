c2c6ee7c6215d2214939e0d0dc5d1af5dbc37ad2
Rogelio Silvan Perez <rogoffspring@hotmail.com>
2018-10-04 11:04:44 +0200

    ejercicios de arrays

---
+++ ejercicios_arrays.js
@@ -0,0 +1,115 @@
+var cadena1 = "aaaaahhsggsbbaaab";
+
+function changeCadena(param1)
+{
+    var nuevoValor="b";
+    var nuevaCadena ="";
+    console.log(param1.length);
+    for (var i = 0; i < param1.length-1; i++) {
+        // nuevaCadena = param1.replace(/b/g, nuevoValor);
+        nuevaCadena = param1.replace(/a/g, nuevoValor);
+        
+    }
+    console.log(nuevaCadena);
+}
+
+
+function borrarChar(param1)
+{
+    var nuevaCadena1 = param1.substr(1,param1.length-2);
+    console.log(nuevaCadena1);
+}
+
+
+function buscarRaton(param1)
+{
+    var ini = param1.indexOf("C");
+    var fin = param1.indexOf("m");
+    var dif = fin - ini;
+    
+    console.log(ini);
+    console.log(fin);
+    console.log(dif);
+    if( dif > 4 )
+        return false;
+    else
+        return true;
+}
+
+
+
+
+function buscarPalabra(param1)
+{
+    var arr_param = param1.split(" ");
+    var cadenaFin = "";
+    for (var i = 0; i < arr_param.length-1; i++) {
+     
+        console.log(arr_param[i]);
+        if(cadenaFin.indexOf(arr_param[i])== -1) cadenaFin += arr_param[i] + " ";
+        
+    }
+    
+    return cadenaFin;
+    
+}
+
+function InvertirCadena(param1)
+{
+    var arr_param = param1.split(" ");
+    var cadenaFin = "";
+    for (var j = 0; j < arr_param.length-1; j++) {
+
+        
+        var palabra = arr_param[j];
+        console.log(arr_param[j]);
+
+        for (var i = palabra.length-1; i >= 0 ; i--) {
+        
+            // console.log(palabra.charAt(i));
+            cadenaFin += palabra[i];
+        }
+
+        cadenaFin += " ";
+    }
+
+    return cadenaFin;
+}
+
+
+let lenguajes = [
+    {
+        name:javascript: 
+        scrore:60
+    },
+    {
+        name:python: 
+        scrore:40
+    },
+    {
+        name: ruby: 
+        scrore:80
+    }];        
+
+function printLanguajes(lenguajes) {
+
+    
+    return lenguajes.filter( condicion => condicion.scrore >= 60).sort(anterior, siguiente) => {
+        if(anterior.score > siguiente.score) return false
+        return true
+    }) 
+
+}    
+
+
+changeCadena("aaaaahhsggsbbaaab");
+borrarChar("hola");
+
+buscarRaton("C.m");
+buscarRaton("C..m");
+buscarRaton("C...m");
+buscarRaton("C....m");
+buscarRaton("...C....m");
+buscarPalabra("barcelona praga barcelona ibiza madrid ibiza madrid");
+
+InvertirCadena("hola que tal estas esto zumbao");
