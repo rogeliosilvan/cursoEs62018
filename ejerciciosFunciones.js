cbbae4f4e44328de99dc2c57838f0cb3f926a61c
Rogelio Silvan Perez <rogoffspring@hotmail.com>
2018-10-15 15:58:21 +0200

    ejercicio de recursividad objetos

--- ejerciciosFunciones.js
+++ ejerciciosFunciones.js
@@ -7,14 +7,83 @@ let projects = [
                 groups: [
                     {
                         name: 'group1.1',
-                        groups: []
+                        groups: [
+                            {
+                                name: 'group1.1.1',
+                                groups: []
+                            }
+                        ]
                     },
                     {
                         name: 'group1.2',
                         groups: [
                             {
                                 name: 'group1.2.1',
-                                groups: []
+                                groups: [
+                                    {
+                                        name: 'group1.2.1.1',
+                                        groups: []
+                                    }
+                                ]
+                            }
+                        ]
+                    }                    
+                ]
+            }
+        ]
+    },
+    {
+        project: 'project 2',
+        groups: []
+    }
+]
+
+let projects2 = [
+    {
+        project: 'project 1',
+        groups: [
+            {
+                name: 'group1',
+                groups: [
+                    {
+                        name: 'group1.1',
+                        groups: [
+                            {
+                                name: 'group1.1.1',
+                                groups: [
+                                    {
+                                        name: 'group1.1.1.1',
+                                        groups: []
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: 'group1.2',
+                        groups: [
+                            {
+                                name: 'group1.2.1',
+                                groups: [
+                                    {
+                                        name: 'group1.2.1.1',
+                                        groups: []
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: 'group1.3',
+                        groups: [
+                            {
+                                name: 'group1.3.1',
+                                groups: [
+                                    {
+                                        name: 'group1.3.1.1',
+                                        groups: []
+                                    }
+                                ]
                             }
                         ]
                     }
@@ -23,23 +92,107 @@ let projects = [
         ]
     },
     {
-        project: 'project2',
-        groups: []
+        project: 'project 2',
+        groups: [
+            {
+                name: 'group2',
+                groups: [
+                    {
+                        name: 'group2.1',
+                        groups: []
+                    }
+                ]
+            }
+        ]
     }
 ]
-function extraerGrupos(grupos) {
+
+var nombreProyectos = [];
+// var una = "";
+// var splitUna = [];
+
+function normalizeGroupV2(projects) {
     
-    return grupos.map(function (group) {
-        
-        if (!group.groups.length)
-            return group.name
-        else
-            return extraerGrupos(group.groups)
-    
-    })
+
+    for (let i = 0; i < projects.length; i++) {
+        nombreProyectos.push({
+            project: projects[i].project,
+            groups: nombreGrupo(projects[i].groups),
+        })
+
+        // una = "";
+        // splitUna = [];
+    }
 }
 
 
+
+function nombreGrupo(grupos) {
+    var nombreGrupo2 = [];
+    
+
+    for (let i = 0; i < grupos.length; i++) {
+        nombreGrupo2.push(grupos[i].name)
+
+        if (grupos[i].groups.length)
+        {
+            var una = "";
+            var splitUna = [];
+            //guardarlo en una variable y juntarlo en un arrya
+            // console.log("Hay mas grupos. Vuelvo a llamar......");
+            // una = una.concat(nombreGrupo(grupos[i].groups));
+            una += nombreGrupo(grupos[i].groups);
+            splitUna = una.split(',');
+            // console.log(una);
+            // console.log(splitUna);
+            nombreGrupo2 = nombreGrupo2.concat(splitUna);
+        }
+
+    }
+
+    return nombreGrupo2;
+}
+
+
+// function extraerGrupos(grupos) {
+
+//     return grupos.map(function (group) {
+
+//         if (!group.groups.length)
+//             return group.name
+//         else
+//             return extraerGrupos(group.groups)
+
+//     })
+// }
+
+function extraerGrupos(grupos) {
+    
+
+    return grupos.map(function (group) {
+
+        var nombreGrupo2 = [];
+        nombreGrupo2.push(group.name)
+        // return group.name
+        if (group.groups.length) {
+            console.log("devuelve grupo");
+
+            var una = "";
+            var splitUna = [];
+            una += extraerGrupos(group.groups);
+            splitUna = una.split(',');
+            console.log("una");
+            console.log(una);
+            console.log("splitUna");
+            console.log(splitUna);
+            return nombreGrupo2.concat(splitUna);
+        }
+
+        return nombreGrupo2;
+
+    })
+
+}
 
 function normalizeGroup(projects) {
     
@@ -53,4 +206,7 @@ function normalizeGroup(projects) {
     
 }
 
-normalizeGroup(projects);
+//normalizeGroup(projects);
+normalizeGroupV2(projects2);
+
+console.log(nombreProyectos)
