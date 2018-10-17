8d2bc53bd0740d8742b12f55e90a407f233f721c
RG <roge@MacBook-Pro-de-RG.local>
2018-10-17 17:15:49 +0200

    cambios en ejerciciosFunciones

--- ejerciciosFunciones.js
+++ ejerciciosFunciones.js
@@ -23,23 +23,100 @@ let projects = [
         ]
     },
     {
-        project: 'project2',
-        groups: []
+        project: 'project 2',
+        groups: [
+            {
+                name: 'group2',
+                groups: [
+                    {
+                        name: 'group2.1',
+                        groups: []
+                    }
+                ]
+            }
+        ]
     }
 ]
-function extraerGrupos(grupos) {
+
+var nombreProyectos = [];
+
+function normalizeGroupV2(projects) {
     
-    return grupos.map(function (group) {
+
+    for (let i = 0; i < projects.length; i++) {
+        nombreProyectos.push({
+            project: projects[i].project,
+            groups: nombreGrupo(projects[i].groups),
+        })
+
+        una = "";
+    }
+}
+
+var acumGrupo = "";
+var una =[];
+
+function nombreGrupo(grupos) {
+    var nombreGrupo2 = [];
+
+    for (let i = 0; i < grupos.length; i++) {
+        nombreGrupo2.push(grupos[i].name)
+        // console.log("name_____");
+        // console.log(grupos[i].name);
+        // console.log("grupo_____");
+        // console.log(grupos[i].groups.length);
+        if (grupos[i].groups.length)
+        {
+            //guardarlo en una variable y juntarlo en un arrya
+            console.log("Hay mas grupos. Vuelvo a llamar......");
+            una = una.concat(nombreGrupo(grupos[i].groups));
+            
+            
+        }
+
+        console.log("acumulo nombre");
+        console.log(una);
+        nombreGrupo2.push(una);
+            
         
-        if (!group.groups.length)
-            return group.name
-        else
-            return extraerGrupos(group.groups)
-    
-    })
+        
+
+    }
+
+    return nombreGrupo2;
 }
 
 
+// function extraerGrupos(grupos) {
+
+//     return grupos.map(function (group) {
+
+//         if (!group.groups.length)
+//             return group.name
+//         else
+//             return extraerGrupos(group.groups)
+
+//     })
+// }
+
+function extraerGrupos(grupos) {
+
+    return grupos.map(function (group) {
+        console.log(group.groups.length);
+        console.log(group.name);
+        var tmpName = [];
+        // return group.name
+        if (!group.groups.length) {
+            console.log("devuelve grupo");
+            tmpName.push(group.name);
+            console.log(tmpName);
+            return group.name
+        }
+
+        extraerGrupos(group.groups);
+
+    })
+}
 
 function normalizeGroup(projects) {
     
@@ -53,4 +130,7 @@ function normalizeGroup(projects) {
     
 }
 
-normalizeGroup(projects);
+// normalizeGroup(projects);
+normalizeGroupV2(projects);
+
+console.log(nombreProyectos)
